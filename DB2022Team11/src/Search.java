dec0227439a1e604a4395818501435062719656a
dawoon08 <lusia08291@gmail.com>
2022-06-01 23:22:58 +0900

    .

--- DB2022Team11/src/Search.java
+++ DB2022Team11/src/Search.java
@@ -164,6 +164,10 @@ public class Search extends JFrame{
 	}catch(SQLException se) {
 		se.printStackTrace();
 	}}
+	
+	public static void main(String[] args) {
+		Search search = new Search();
+	}
 
 }
 
