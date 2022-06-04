19eb0058425928ac266a058826c0b6d651ffee2d
dawoon08 <lusia08291@gmail.com>
2022-06-05 01:05:10 +0900

    view 수정

--- create_view.sql
+++ create_view.sql
@@ -1,21 +1,21 @@
 
 create view DB2022_daehyun_dong  as 
-select DB2022_area.area_id, area_dong, building_name, building_type, building_id
+select DB2022_area.area_id, area_gu, area_dong, building_name, building_type, building_id
 from DB2022_AREA, DB2022_BUILDING
 where DB2022_AREA.area_id = 'Lid001' and DB2022_BUILDING.area_id = 'Lid001';
 
 create view DB2022_sinchon_dong  as 
-select DB2022_area.area_id, area_dong, building_name, building_type, building_id
+select DB2022_area.area_id, area_gu, area_dong, building_name, building_type, building_id
 from DB2022_AREA, DB2022_BUILDING
 where DB2022_AREA.area_id = 'Lid002' and DB2022_BUILDING.area_id = 'Lid002';
 
 create view DB2022_sinsa_dong  as 
-select DB2022_area.area_id, area_dong, building_name, building_type, building_id
+select DB2022_area.area_id, area_gu, area_dong, building_name, building_type, building_id
 from DB2022_AREA, DB2022_BUILDING
 where DB2022_AREA.area_id = 'Lid003' and DB2022_BUILDING.area_id = 'Lid003';
 
 create view DB2022_yeonnam_dong  as 
-select DB2022_area.area_id, area_dong, building_name, building_type, building_id
+select DB2022_area.area_id, area_gu, area_dong, building_name, building_type, building_id
 from DB2022_AREA, DB2022_BUILDING
 where DB2022_AREA.area_id = 'Lid004' and DB2022_BUILDING.area_id = 'Lid004';
 
